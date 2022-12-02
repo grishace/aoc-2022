1ec7f6be85d4201d8f4b8c6f9854559c2b1cfc99
Grigoriy Belenkiy <grishace@gmail.com>
2022-12-01 18:17:55 -0700

    Day 1

---
+++ README.md
@@ -0,0 +1,3 @@
+# Advent of Code 2022
+
+* [Day 1: Calorie Counting](https://adventofcode.com/2022/day/1) [F#](https://github.com/grishace/aoc-2022)
---
+++ day01-calorie-counting/Program.fs
@@ -0,0 +1,21 @@
+﻿open System
+open System.IO
+
+let input =
+    File.ReadAllLines "input.txt"
+    |> Seq.ofArray
+
+let split input =
+  let mutable i = 0
+  input
+  |> Seq.map  (fun x ->
+    if x = "" then i <- i + 1
+    i, x)
+  |> Seq.groupBy fst
+  |> Seq.map (fun (_, b) -> Seq.map snd b |> Seq.filter (fun x -> x <> "") |> Seq.map int)
+
+let calories = input |> split |> Seq.map (Seq.sum)
+
+printfn "Part 1: %i" (calories |> Seq.max)
+
+printfn "Part 2: %i" (calories |> Seq.sortDescending |> Seq.take 3 |> Seq.sum)
