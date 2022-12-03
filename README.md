2d05110d1ef7e0377a48be36f512d5543015c350
Grigoriy Belenkiy <grishace@gmail.com>
2022-12-03 09:34:33 -0700

    Day 3

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 
 * [Day 1: Calorie Counting](https://adventofcode.com/2022/day/1) [F#](https://github.com/grishace/aoc-2022/blob/main/day01-calorie-counting/Program.fs)
 * [Day 2: Rock Paper Scissors](https://adventofcode.com/2022/day/2) [F#](https://github.com/grishace/aoc-2022/blob/main/day02-rock-paper-scissors/Program.fs)
+* [Day 3: Rucksack Reorganization](https://adventofcode.com/2022/day/3) [F#](https://github.com/grishace/aoc-2022/blob/main/day03-rucksack-reorganization/Program.fs)
---
+++ day03-rucksack-reorganization/Program.fs
@@ -0,0 +1,30 @@
+﻿open System.IO
+
+let input =
+    File.ReadAllLines "input.txt"
+    |> Seq.ofArray
+
+let priority = function
+    | lc when lc >= 'a' && lc <= 'z' -> int lc - int 'a' + 1
+    | uc when uc >= 'A' && uc <= 'Z' -> int uc - int 'A' + 27
+    | _ -> failwith "error"
+
+let totalPriority s = s |> Set.map priority |> Set.toSeq |> Seq.sum
+
+let rucksacks =
+    input
+    |> Seq.map (fun s -> Set.ofSeq (s.Substring(0, s.Length/2)), Set.ofSeq (s.Substring(s.Length/2, s.Length - s.Length/2)))
+    |> Seq.map (fun (c1, c2) -> Set.intersect c1 c2)
+    |> Seq.map totalPriority
+    |> Seq.sum
+
+printfn "Part 1: %i" rucksacks
+
+let badges =
+    input
+    |> Seq.chunkBySize 3
+    |> Seq.map (fun s -> Set.intersect (Set.intersect (Set.ofSeq s.[0]) (Set.ofSeq s.[1])) (Set.ofSeq s.[2]))
+    |> Seq.map totalPriority
+    |> Seq.sum
+
+printfn "Part 2: %i" badges
