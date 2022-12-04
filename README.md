f143f2f5866e38bbc9b994a45317fa03e85a2f3e
Grigoriy Belenkiy <grishace@gmail.com>
2022-12-03 22:42:27 -0700

    Day 4

--- README.md
+++ README.md
@@ -3,3 +3,4 @@
 * [Day 1: Calorie Counting](https://adventofcode.com/2022/day/1) [F#](https://github.com/grishace/aoc-2022/blob/main/day01-calorie-counting/Program.fs)
 * [Day 2: Rock Paper Scissors](https://adventofcode.com/2022/day/2) [F#](https://github.com/grishace/aoc-2022/blob/main/day02-rock-paper-scissors/Program.fs)
 * [Day 3: Rucksack Reorganization](https://adventofcode.com/2022/day/3) [F#](https://github.com/grishace/aoc-2022/blob/main/day03-rucksack-reorganization/Program.fs)
+* [Day 4: Camp Cleanup](https://adventofcode.com/2022/day/4) [F#](https://github.com/grishace/aoc-2022/blob/main/day04-camp-cleanup/Program.fs)
---
+++ day04-camp-cleanup/Program.fs
@@ -0,0 +1,27 @@
+﻿open System.IO
+
+let input =
+    File.ReadAllLines "input.txt"
+    |> Seq.ofArray
+
+let ranges =
+    input
+    |> Seq.map (fun s -> s.Split ",")
+    |> Seq.map (fun x ->
+        let toRange (s: string) = s.Split "-" |> Array.map int |> (fun x -> seq { for i in x.[0] .. x.[1] do i } |> Set.ofSeq)
+        toRange x.[0], toRange x.[1]
+        )
+    |> Seq.map (fun (s1, s2) ->
+        let overlap = Set.intersect s1 s2
+        overlap, s1, s2
+        )
+
+let assignments =
+    ranges |> Seq.filter (fun (o, s1, s2) -> o.Count = s1.Count || o.Count = s2.Count)
+
+printfn "Part 1: %i" <| Seq.length assignments
+
+let overlaps =
+    ranges |> Seq.filter (fun (o, _, _) -> o.Count > 0)
+
+printfn "Part 2: %i"  <| Seq.length overlaps
