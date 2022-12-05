fffd7338d5babc584a8e6780ea52036e44e4c180
Grigoriy Belenkiy <grishace@gmail.com>
2022-12-05 00:14:41 -0700

    Day 5

--- README.md
+++ README.md
@@ -4,3 +4,4 @@
 * [Day 2: Rock Paper Scissors](https://adventofcode.com/2022/day/2) [F#](https://github.com/grishace/aoc-2022/blob/main/day02-rock-paper-scissors/Program.fs)
 * [Day 3: Rucksack Reorganization](https://adventofcode.com/2022/day/3) [F#](https://github.com/grishace/aoc-2022/blob/main/day03-rucksack-reorganization/Program.fs)
 * [Day 4: Camp Cleanup](https://adventofcode.com/2022/day/4) [F#](https://github.com/grishace/aoc-2022/blob/main/day04-camp-cleanup/Program.fs)
+* [Day 5: Supply Stacks](https://adventofcode.com/2022/day/5) [F#](https://github.com/grishace/aoc-2022/blob/main/day05-supply-stacks/Program.fs)
---
+++ day05-supply-stacks/Program.fs
@@ -0,0 +1,47 @@
+﻿open System
+open System.IO
+open System.Text.RegularExpressions
+
+let input =
+    File.ReadAllLines "input.txt"
+
+let emptyLine = input |> Array.findIndex (fun s -> String.IsNullOrEmpty(s))
+
+let numberOfStacks = 
+    input.[emptyLine-1].Split (' ', StringSplitOptions.RemoveEmptyEntries)
+    |> Array.map int
+    |> Array.max
+
+let crates ()= [|
+    for i in 0 .. (numberOfStacks-1) do
+        yield  [
+            for l in (emptyLine-2) .. -1 .. 0 do
+                let c = input.[l].[i*4+1]
+                if c <> ' ' then yield c
+        ] |> List.rev
+    |]
+
+let rx = Regex("move (\d+) from (\d+) to (\d+)", RegexOptions.Compiled)
+
+let commands  = 
+    input.[emptyLine+1..input.Length-1]
+    |> Array.map (fun s -> 
+        let m = rx.Match s
+        int m.Groups.[1].Value, int m.Groups.[2].Value, int m.Groups.[3].Value
+        )
+
+let result fn = 
+    commands 
+    |> Array.fold (fun (c: char list[]) (n, f, t) ->
+        let toMove = c.[f-1].[..n-1] |> fn
+        let remains = c.[f-1].[n..]
+        c.[t-1] <- toMove @ c.[t-1]
+        c.[f-1] <- remains
+        c
+        ) (crates())
+    |> Array.map (fun l -> l |> List.head)
+    |> String
+
+printfn "Part 1: %s" (result  List.rev)
+
+printfn "Part 2: %s" (result id)
